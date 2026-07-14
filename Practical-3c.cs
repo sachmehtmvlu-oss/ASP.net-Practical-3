73d5900e97ca0600eda4b2b179e90113b11061e9
sachmehtmvlu-oss <sachmehtmvlu@gmail.com>
2026-07-14 09:47:47 +0530

    Add files via upload

---
+++ Practical-3c.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class BankAcc
+{
+    public int AccNumber { get; set; }
+
+    private double balance;
+
+    public double Balance
+    {
+        get
+        {
+            return balance;
+        }
+
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Balance cannot be negative.");
+            }
+            else
+            {
+                balance = value;
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankAcc account = new BankAcc();
+
+        account.AccNumber = 345;
+        account.Balance = 1000;
+
+        Console.WriteLine("Account Number: " + account.AccNumber);
+        Console.WriteLine("Balance: " + account.Balance);
+        // Attempt to set a negative balance
+        account.Balance = -500;
+
+        Console.WriteLine("Balance after attempting to set negative value: " + account.Balance);
+
+        Console.WriteLine("Sachin Ramesh Mehta");
+
+    }
+}
