5a74aae04c30cef26ce8014afa48458b7d3f39d3
sachmehtmvlu-oss <sachmehtmvlu@gmail.com>
2026-07-14 09:31:01 +0530

    Add files via upload

---
+++ Practical 3a.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+delegate void operation(int a, int b);
+class Calculator
+{
+    public void add(int a, int b)
+    {
+        Console.WriteLine("Addition of number :" + (a + b));
+    }
+    public void sub(int a, int b)
+    {
+        Console.WriteLine("Subtraction of number :" + (a - b));
+    }
+    public void mul(int a, int b)
+    {
+        Console.WriteLine("Multiplication of number :" + (a * b));
+    }
+    public void div(int a, int b)
+    {
+        Console.WriteLine("Division of number :" + (a / b));
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Calculator c = new Calculator();
+        operation obj;
+
+        obj = c.add;
+        obj(14, 14);
+
+        obj = c.sub;
+        obj(14, 14);
+
+        obj = c.mul;
+        obj(14, 14);
+
+        obj = c.div;
+        obj(14, 14);
+
+        Console.WriteLine("Sachin Ramesh Mehta");
+    }
+}
+
