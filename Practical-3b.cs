39bee78f25979e444628ac2a9a1cd574acd3817f
sachmehtmvlu-oss <sachmehtmvlu@gmail.com>
2026-07-14 09:28:21 +0530

    Add files via upload

---
+++ Practical-3b.cs
@@ -0,0 +1,33 @@
+﻿using System;
+delegate void Message();
+class Demo
+{
+    public void Welcome()
+
+    {
+        Console.WriteLine("Welcome everyone");
+    }
+    public void Learn()
+    {
+        Console.WriteLine("We will learn C# Delegates");
+    }
+    public void thanku()
+    {
+        Console.WriteLine("Thank you everyone");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Demo d = new Demo();
+        Message msg;
+        msg  = d.Welcome;
+        msg += d.Learn;
+        msg += d.thanku;
+        msg();
+        Console.WriteLine("Sachin Ramesh Mehta");
+    }
+}
+
